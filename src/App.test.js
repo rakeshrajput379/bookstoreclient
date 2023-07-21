b849c0110906adaf3362623d06a7bcaec050536b
poojarajput22415 <poojarajput22415@gmail.com>
2023-07-21 12:37:16 +0530

    changes in main

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { Provider } from "react-redux";
 
 describe("App Component", () => {
   it("should render app with error", () => {
-    const { asFragment } = render(<App></App>);
+    const { asFragment } = render(<Provider></Provider>);
 
     expect(asFragment()).toMatchSnapshot();
   });
